778110fb57af365bc5fadf5a4d37d4698d568e4d
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-10-10 16:21:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,20 @@
-
-import React, { useState } from "react";
-import '../styles/App.css';
-import Selection from './Selection';
+import React, { useState } from 'react';
 import ColourSelector from './ColourSelector';
-
-const colourConfig = [
-  {
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
-  },
-  {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
-  },
-  {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
-]
-
-const title = 'Select the gradient and then the Box to change the color';
+import Selection from './Selection';
 
 const App = () => {
-  const [nextBackground, selectNextBackground] = useState({ background: "" });
+    const [selectedColor, setSelectedColor] = useState('');
 
-  const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground);
-  }
+    const handleColorChange = (color) => {
+        setSelectedColor(color);
+    };
 
-  return (
-    <div id="master">
-      <h5 className="heading">{title}</h5>  {/* ✅ display title here */}
-
-      <div className="row">
-        {colourConfig.map((config) => (
-          <ColourSelector
-            key={config.key}
-            config={config}
-            selectNextBackground={selectNextBackground}
-          />
-        ))}
-      </div>
-
-      <div className='row' id="children-wrapper">
-        {["selection1", "selection2", "selection3"].map((key) => (
-          <Selection key={key} applyColor={applyColor} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <ColourSelector onColorChange={handleColorChange} />
+            <Selection selectedColor={selectedColor} />
+        </div>
+    );
 };
 
 export default App;
