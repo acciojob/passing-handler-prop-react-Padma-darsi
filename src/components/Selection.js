21826656bfedc44f98ae601e0e6441d577032cb3
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 18:16:34 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,27 @@
+import React, { useState } from "react";
+
+const Selection = ({ selectedColor }) => {
+  const [bgColor, setBgColor] = useState("");
+
+  const handleClick = () => {
+    setBgColor(selectedColor);
+  };
+
+  return (
+    <div
+      className="fix-box"
+      onClick={handleClick}
+      style={{
+        width: "100px",
+        height: "100px",
+        border: "2px solid black",
+        margin: "10px",
+        backgroundColor: bgColor,
+        cursor: "pointer",
+      }}
+    ></div>
+  );
+};
+
+export default Selection;
 
