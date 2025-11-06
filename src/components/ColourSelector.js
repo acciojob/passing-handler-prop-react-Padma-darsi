cf68afd96f35ff39aea522b08746f927644c08ce
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 18:03:39 +0530

    Update ColourSelector.js

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,18 +1,4 @@
-import React from "react";
 
-const ColourSelector = ({ color, applyColor }) => {
-  return (
-    <button
-      className="btn"
-      style={{ backgroundColor: color.value, color: "white", margin: "5px" }}
-      onClick={() => applyColor(color.value)}
-    >
-      {color.label}
-    </button>
-  );
-};
-
-export default ColourSelector;
 
 
 
